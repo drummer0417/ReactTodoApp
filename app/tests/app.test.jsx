0feaf33cda76462a2cc327da05c20c1ebae91552
drummer0417 <hans@androidappfactory.nl>
2017-06-14 11:38:50 +0200

    Initial commit

---
+++ app/tests/app.test.jsx
@@ -0,0 +1,8 @@
+var expect = require('expect');
+
+describe('App', () => {
+  it('Should run an useless test just to test the test.', () => {
+    expect(1).toBe(1);
+  });
+
+});
---
+++ server.js
@@ -0,0 +1,21 @@
+var express = require('express');
+
+// Create our app
+var app = express();
+var port = process.env.PORT || 3000;
+
+app.use(function(req, res, next) {
+  if(req.headers['x-forwarded-proto'] === 'https') {
+    // console.log('is https');
+    res.redirect('http://' + req.hostname + req.url);
+  } else {
+    // console.log('is not https');
+    next();
+  }
+});
+
+app.use(express.static('public'));
+
+app.listen(port, function() {
+  console.log(`Express server is up on port ${port}`);
+});
