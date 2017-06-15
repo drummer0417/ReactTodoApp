3722ccca84519de3ac2804bbc8800880fbe1a1bd
drummer0417 <hans@androidappfactory.nl>
2017-06-15 16:40:08 +0200

    Just to save it....

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -4,6 +4,15 @@ var Test = require('Test');
 
 var TodoApp = React.createClass({
 
+  getInitialState: function() {
+    return {
+      todos: [
+        { id: 1, text: 'Walk he dog' },
+        { id: 2, text: 'Get a coffee' },
+        { id: 3, text: 'Have lunch, get a sandwich' },
+      ]
+    }
+  },
   render: function() {
     return(
       <div>
