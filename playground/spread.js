752429e1685228ae4edf4d1b0eb319bf4126b383
drummer0417 <hans@androidappfactory.nl>
2017-06-18 13:01:09 +0200

    Added playground for spread operator

---
+++ playground/spread.js
@@ -0,0 +1,28 @@
+// function add (a, b) {   return a + b; }
+//
+// console.log(add(3, 4));
+//
+// var c = [3, 5];
+//
+// console.log(add(...c)); var groupA = ['Hans', 'Jacky']; var groupB = ['Cas', 'Anouk'];
+//
+// var final = [...groupA, 3, ...groupB];
+//
+// console.log(final);
+
+var p1 = ['Hans', 55];
+var p2 = ['Jacky', 50];
+
+function sayHello(name, age) {
+  console.log('My name is', name, 'and i\'m', age);
+}
+
+sayHello(...p1);
+sayHello(...p2);
+
+var names = ['Hans', 'Jacky'];
+var allNames = ['Cas', ...names,  'Anouk'];
+
+allNames.forEach( (name) => {
+  console.log('Hi', name);
+})
